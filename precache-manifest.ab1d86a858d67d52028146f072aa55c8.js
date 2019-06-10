6e6a7e8f801edfe58a4bf62f51fa061838f53fde
Iwona Antonik <iwona.antonik@gmail.com>
2019-06-09 21:56:25 -0400

    Updates

---
+++ precache-manifest.ab1d86a858d67d52028146f072aa55c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06cccabda88f1c2f3c9102fe982b6d3",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "95ca94538c737a50d379",
+    "url": "/memory-game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "df8008faacfc41c65321",
+    "url": "/memory-game/static/css/main.1b2b9f8f.chunk.css"
+  },
+  {
+    "revision": "95ca94538c737a50d379",
+    "url": "/memory-game/static/js/2.98a4c18b.chunk.js"
+  },
+  {
+    "revision": "df8008faacfc41c65321",
+    "url": "/memory-game/static/js/main.55db1bde.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
